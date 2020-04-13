092c95aa55f004df876186ab8e229b91b269715d
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-04-14 01:49:18 +0200

    clean script

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,29 @@
 
 import React from 'react';
-import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div` 
+    width: 60%;
+    margin: 16px auto;
+    border: solid 1px #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center; 
+
+ @media(min-width: 500px){ 
+        width: 450px;
+        color: #DCC6F2 
+} 
+`;
 
 const Person = (props) => { 
-
+ 
     return (
-        <div className="Person">
+        <StyledDiv> 
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} /> 
-        </div> 
+        </StyledDiv> 
     )
 }
 
