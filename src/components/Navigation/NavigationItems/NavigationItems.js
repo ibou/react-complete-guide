5a1c4c015dceca8c282fad4629f9de7ee772f9b2
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-04-24 00:47:31 +0200

    Final burger menu

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,8 +2,6 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-
-
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active='true'>Burger Builder</NavigationItem>
